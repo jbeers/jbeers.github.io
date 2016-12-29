85d8fd5982b413c329b37ecb5fe9a69bfc481d1c
jbeers <jbeers@diastolicdesign.com>
2016-12-29 16:39:02 -0600

    Create threeTest.js

---
+++ threeTest.js
@@ -0,0 +1,43 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.getElementById( 'threewrapper' ).appendChild( renderer.domElement );
+
+var geometry = new THREE.OctahedronGeometry( 1, 0 );
+var material = new THREE.MeshPhongMaterial( {
+					color: 0x00ff00,
+					emissive: 0x072534,
+					side: THREE.DoubleSide,
+					shading: THREE.FlatShading
+				} )
+var cube = new THREE.Mesh( geometry, material );
+
+var increment = Math.PI / 200;
+
+scene.add( cube );
+var lights = [];
+			lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+			lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+			lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+
+			lights[ 0 ].position.set( 0, 200, 0 );
+			lights[ 1 ].position.set( 100, 200, 100 );
+			lights[ 2 ].position.set( - 100, - 200, - 100 );
+
+			scene.add( lights[ 0 ] );
+			scene.add( lights[ 1 ] );
+			scene.add( lights[ 2 ] );
+
+
+camera.position.z = 5;
+camera.position.y = 1.8;
+camera.rotation.x = -Math.PI / 8;
+
+function render() {
+	cube.rotation.y += increment;
+	requestAnimationFrame( render );
+	renderer.render( scene, camera );
+}
+render();
