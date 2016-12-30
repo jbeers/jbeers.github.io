af611594f8bcfe666c39582c7ead9d64d4c36907
jbeers <jbeers@diastolicdesign.com>
2016-12-30 09:55:39 -0600

    Update threeTest.js

--- threeTest.js
+++ threeTest.js
@@ -7,28 +7,31 @@ document.getElementById( 'threewrapper' ).appendChild( renderer.domElement );
 
 var geometry = new THREE.OctahedronGeometry( 1, 0 );
 var material = new THREE.MeshPhongMaterial( {
-					color: 0x00ff00,
-					emissive: 0x072534,
-					side: THREE.DoubleSide,
-					shading: THREE.FlatShading
-				} )
+	color: 0x00ff00,
+	emissive: 0x072534,
+	side: THREE.DoubleSide,
+	shading: THREE.FlatShading
+} );
 var cube = new THREE.Mesh( geometry, material );
 
 var increment = Math.PI / 200;
 
 scene.add( cube );
+
+var orbit = new THREE.OrbitControls( camera, renderer.domElement );
+orbit.enableZoom = false;
 var lights = [];
-			lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
-			lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
-			lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
 
-			lights[ 0 ].position.set( 0, 200, 0 );
-			lights[ 1 ].position.set( 100, 200, 100 );
-			lights[ 2 ].position.set( - 100, - 200, - 100 );
+lights[ 0 ].position.set( 0, 200, 0 );
+lights[ 1 ].position.set( 100, 200, 100 );
+lights[ 2 ].position.set( - 100, - 200, - 100 );
 
-			scene.add( lights[ 0 ] );
-			scene.add( lights[ 1 ] );
-			scene.add( lights[ 2 ] );
+scene.add( lights[ 0 ] );
+scene.add( lights[ 1 ] );
+scene.add( lights[ 2 ] );
 
 
 camera.position.z = 5;
