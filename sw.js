39ecb31c949623a574a07972422334dc55aac1ea
Jacob Beers <jbeers@diastolicdesign.com>
2019-03-27 17:30:41 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,8 +11,12 @@ self.addEventListener( 'activate', async function( e ){
 	console.log( 'activated' )
 	
 	setTimeout(() => {
-		displayNotification( 'Counter', { body: 'A push notificatin!' });
+		displayNotification( 'Counter', { body: 'A push notification!' });
 	}, 5000 );
+	
+	setTimeout(() => {
+		displayNotification( 'Counter', { body: 'This one happened later!' });
+	}, 55000 );
 });
 
 console.log( 'test' );
