7382b99f74e5a69004607074216f4dff0140181e
Jacob Beers <jbeers@diastolicdesign.com>
2019-03-27 17:26:33 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,3 @@
-let counter = 0;
-
 function displayNotification( title, options ){
 	if( self.registration && self.registration.showNotification ){
 		self.registration.showNotification( title, options );
@@ -12,9 +10,8 @@ function displayNotification( title, options ){
 self.addEventListener( 'activate', async function( e ){
 	console.log( 'activated' )
 	
-	setInterval(() => {
-		displayNotification( 'Counter', { body: 'The counter is now: ' + counter });
-		counter += 1;
+	setTimeout(() => {
+		displayNotification( 'Counter', { body: 'A push notificatin!' });
 	}, 5000 );
 });
 
